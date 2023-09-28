42536fa67581ffaa5a2824656685f35029de2cad
jroquebento <jroquebento@gmail.com>
2023-09-28 10:20:45 -0300

    classes Comparator e método sort

--- list/src/ExemploOrdenacaoList.java
+++ list/src/ExemploOrdenacaoList.java
@@ -6,26 +6,36 @@ public class ExemploOrdenacaoList {
     public static void main(String[] args) {
         
         List<Gato> meusGatos = new ArrayList<>(){{
-            add(new Gato("Jon", 18, "preto"));
+            add(new Gato("Jon", 12, "preto"));
             add(new Gato("Simba", 6, "tigrado"));
             add(new Gato("Jon", 18, "amarelo"));
         }};
 
-        System.out.println();
-
-        System.out.println("--\tOrdem de Inserção\t---");
+        System.out.println("\n--\tOrdem de Inserção\t---");
         System.out.println(meusGatos);
 
         System.out.println();
-        System.out.println("--\tOrdem aleatório\t---");
+        System.out.println("\n--\tOrdem aleatório\t---");
         // shuffle - embaralha os elementos
         Collections.shuffle(meusGatos);
         System.out.println(meusGatos);
 
-        System.out.println("--\tOrdem alfabética\t---");
+        System.out.println("\n--\tOrdem alfabética\t---");
         // sort - ordena os elementos na condição solicitada
         Collections.sort(meusGatos);
         System.out.println(meusGatos);
         
+        System.out.println("\n--\tOrdem idade\t---");   
+        Collections.sort(meusGatos, new ComparatorIdade());
+        System.out.println(meusGatos);
+
+        System.out.println("\n--\tOrdem cor\t---");   
+        meusGatos.sort(new ComparatorCor());
+        System.out.println(meusGatos);
+
+        System.out.println("\n--\tOrdem Nome/Cor/Idade\t---");  
+        meusGatos.sort(new ComparatorNomeCorIdade());
+        System.out.println(meusGatos);
+
     }
 }
