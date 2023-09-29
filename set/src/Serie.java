8a144758fb69e50b285cc3ba8d6f0b852c784d94
jroquebento <jroquebento@gmail.com>
2023-09-29 20:42:34 -0300

    Set - ordenação, compareTo e Comparator

---
+++ set/src/Serie.java
@@ -0,0 +1,70 @@
+import java.util.Objects;
+
+public class Serie implements Comparable<Serie>{
+    private String nome;
+    private String genero;
+    private Integer tempoEpisodio;
+    
+    public Serie(String nome, String genero, Integer tempoEpisodio) {
+        this.nome = nome;
+        this.genero = genero;
+        this.tempoEpisodio = tempoEpisodio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public Integer getTempoEpisodio() {
+        return tempoEpisodio;
+    }
+
+    @Override
+    public String toString() {
+        return "Serie [nome=" + nome + ", genero=" + genero + ", tempoEpisodio=" + tempoEpisodio + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, genero, tempoEpisodio);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Serie other = (Serie) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (genero == null) {
+            if (other.genero != null)
+                return false;
+        } else if (!genero.equals(other.genero))
+            return false;
+        if (tempoEpisodio == null) {
+            if (other.tempoEpisodio != null)
+                return false;
+        } else if (!tempoEpisodio.equals(other.tempoEpisodio))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo(Serie serie) { 
+        int tempoEpisodio = Integer.compare(this.getTempoEpisodio(), serie.getTempoEpisodio());
+        if(tempoEpisodio != 0) return tempoEpisodio;
+        
+        return this.getGenero().compareTo(serie.getGenero());   
+    }
+}
