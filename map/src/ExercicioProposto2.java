82d48f26abec041260327f0f03c3d5c270ab7386
jroquebento <jroquebento@gmail.com>
2023-10-03 16:00:26 -0300

    lançar dados - exercício proposto 2

---
+++ map/src/ExercicioProposto2.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class ExercicioProposto2 {
+    public static void main(String[] args) {
+        Integer quantidadeLancamentos = 100;
+
+        Random numeroAleatorio = new Random();
+        List<Integer> valores = new ArrayList<>();
+
+        for(int i = 1; i <= quantidadeLancamentos; i++){
+            valores.add(numeroAleatorio.nextInt(7));
+        }
+
+        Map<Integer, Integer> lancamentos = new HashMap<>();
+        for (Integer resultado : valores) {
+            if(lancamentos.containsKey(resultado)){
+                lancamentos.put(resultado, lancamentos.get(resultado) + 1);
+            }
+            else{
+                lancamentos.put(resultado, 1);
+            }
+        }
+        
+        System.out.println("\nValor " + " Quant. de vezes");
+        for(Map.Entry<Integer, Integer> entry : lancamentos.entrySet()){
+            System.out.printf("%3d %10d\n", entry.getKey(), entry.getValue());
+        }
+    }
+}
